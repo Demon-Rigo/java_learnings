d2905fb306898947275665667398e20679e4b736
Ajaj Shakib <93483910+Demon-Rigo@users.noreply.github.com>
2024-09-12 14:33:54 +0000

    implimenting java code to print integers from 1 to 100 in a sequence

---
+++ 01_Basics/04_print_integers.java
@@ -0,0 +1,10 @@
+class show_integers {
+    public static void main(String[] args) {
+        
+        for(int number = 1; number<=100; number++)
+       
+        System.out.print(number + " ");
+    }
+}
+
+// this is a code in java to print integers from 1 to 100 in a sequence
